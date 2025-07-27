76594185b4f34f1a53da61f896d1e4ae892fd824
marychka2335 <marychka2335@icloud.com>
2025-07-27 13:53:27 +0300

    Add Dockerized Django project with PostgreSQL and Nginx

---
+++ settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
